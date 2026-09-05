ad06b0b8553fd4999c169dc15830fd95a1893731
Huttman Ochoa <huttman@hotmail.com>
2026-09-05 09:02:45 -0600

    Import redirect from flask

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt, get_jwt_identity
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
